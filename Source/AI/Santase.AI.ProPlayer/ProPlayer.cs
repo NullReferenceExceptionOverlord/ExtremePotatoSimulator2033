49abf41c8dba52d0a8c62570ea1b3d8c15b75b77
TestTestTestTest <ftw@mail.bg>
2015-12-05 20:30:35 +0200

    Fixed spaces issues

--- Source/AI/Santase.AI.ProPlayer/ProPlayer.cs
+++ Source/AI/Santase.AI.ProPlayer/ProPlayer.cs
@@ -1,65 +1,64 @@
 ﻿namespace Santase.AI.ProPlayer
 {
     using System;
-    using System.Linq;
-    using System.IO;
     using System.Collections.Generic;
-
+    using System.IO;
+    using System.Linq;
     using Logic.Cards;
     using Logic.Extensions;
-    using Logic.Players;
     using Logic.PlayerActionValidate;
+    using Logic.Players;
     using Tools.Extensions;
 
     public class ProPlayer : IPlayer
     {
-		public ProPlayer()
+        public ProPlayer()
         {
-			this.AnnounceValidator = new AnnounceValidator();
-			this.PlayerActionValidator = new PlayerActionValidator();
+            this.AnnounceValidator = new AnnounceValidator();
+            this.PlayerActionValidator = new PlayerActionValidator();
         }
 
-		protected bool IsFirstPlayer { get; set; }
+        protected bool IsFirstPlayer { get; set; }
 
-		protected int MyPoints { get; set; }
+        protected int MyPoints { get; set; }
 
-		protected int OpponentPoints { get; set; }
+        protected int OpponentPoints { get; set; }
 
-		protected CardMemorizer CardMemorizer { get; set; }
+        protected CardMemorizer CardMemorizer { get; set; }
 
-		protected IReadOnlyCollection<Card> MyHand
-		{
-			get
-			{
-				return this.CardMemorizer.MyHand;
-			}
-		}
-
-		protected IAnnounceValidator AnnounceValidator { get; }
-
-		protected IPlayerActionValidator PlayerActionValidator { get; }
-
-		public string Name => "Potato!";
-
-		public virtual void StartGame(string otherPlayerIdentifier)
-		{
-		}
-
-		public virtual void StartRound(ICollection<Card> cards, Card trumpCard, int myTotalPoints, int opponentTotalPoints)
-		{
-			this.CardMemorizer = new CardMemorizer(trumpCard, cards);
-		}
-
-		public virtual void AddCard(Card card)
-		{
-			this.CardMemorizer.LogDrawnCard(card);
-		}
-
-		public virtual PlayerAction GetTurn(PlayerTurnContext context)
+        protected IReadOnlyCollection<Card> MyHand
         {
-			this.IsFirstPlayer = context.IsFirstPlayerTurn;
+            get
+            {
+                return this.CardMemorizer.MyHand;
+            }
+        }
 
-			var myHand = this.CardMemorizer.GetMyHand();
+        protected IAnnounceValidator AnnounceValidator { get; }
+
+        protected IPlayerActionValidator PlayerActionValidator { get; }
+
+        public string Name => "Potato!";
+
+        public virtual void StartGame(string otherPlayerIdentifier)
+        {
+        }
+
+        public virtual void StartRound(ICollection<Card> cards, Card trumpCard, int myTotalPoints, int opponentTotalPoints)
+        {
+            this.CardMemorizer = new CardMemorizer(trumpCard, cards);
+        }
+
+        public virtual void AddCard(Card card)
+        {
+            this.CardMemorizer.LogDrawnCard(card);
+        }
+
+        public virtual PlayerAction GetTurn(PlayerTurnContext context)
+        {
+            this.IsFirstPlayer = context.IsFirstPlayerTurn;
+
+            var myHand = this.CardMemorizer.GetMyHand();
 
             if (this.PlayerActionValidator.IsValid(PlayerAction.ChangeTrump(), context, myHand))
             {
@@ -71,10 +70,10 @@
                 return this.CloseGame();
             }
 
-			if (context.CardsLeftInDeck == 0)
-			{
-				this.CardMemorizer.LogTrumpCardDrawn();
-			}
+            if (context.CardsLeftInDeck == 0)
+            {
+                this.CardMemorizer.LogTrumpCardDrawn();
+            }
 
             var possibleCardsToPlay = this.PlayerActionValidator.GetPossibleCardsToPlay(context, myHand);
             var cardToPlay = possibleCardsToPlay.First();
@@ -89,32 +88,32 @@
             return this.PlayCard(cardToPlay);
         }
 
-		public virtual void EndTurn(PlayerTurnContext context)
+        public virtual void EndTurn(PlayerTurnContext context)
         {
-			if (!this.CardMemorizer.TrumpCardDrawn && !this.CardMemorizer.TrumpCard.Equals(context.TrumpCard))
-			{
-				this.CardMemorizer.LogTrumpChange();
-			}
+            if (!this.CardMemorizer.TrumpCardDrawn && !this.CardMemorizer.TrumpCard.Equals(context.TrumpCard))
+            {
+                this.CardMemorizer.LogTrumpChange();
+            }
 
-			Card opponentCard = context.SecondPlayedCard;
+            Card opponentCard = context.SecondPlayedCard;
             if (this.IsFirstPlayer)
-			{
-				this.MyPoints = context.FirstPlayerRoundPoints;
-				this.OpponentPoints = context.SecondPlayerRoundPoints;
-			}
-			else
-			{
-				this.MyPoints = context.SecondPlayerRoundPoints;
-				this.OpponentPoints = context.FirstPlayerRoundPoints;
+            {
+                this.MyPoints = context.FirstPlayerRoundPoints;
+                this.OpponentPoints = context.SecondPlayerRoundPoints;
+            }
+            else
+            {
+                this.MyPoints = context.SecondPlayerRoundPoints;
+                this.OpponentPoints = context.FirstPlayerRoundPoints;
 
-				opponentCard = context.FirstPlayedCard;
-			}
+                opponentCard = context.FirstPlayedCard;
+            }
 
-			if (opponentCard != null)
-			{
-				this.CardMemorizer.LogOpponentPlayedCard(opponentCard);
-			}
-		}
+            if (opponentCard != null)
+            {
+                this.CardMemorizer.LogOpponentPlayedCard(opponentCard);
+            }
+        }
 
         public virtual void EndRound()
         {
@@ -124,40 +123,40 @@
         {
         }
 
-		protected PlayerAction ChangeTrump()
-		{
-			this.CardMemorizer.LogTrumpChange();
-			return PlayerAction.ChangeTrump();
-		}
+        protected PlayerAction ChangeTrump()
+        {
+            this.CardMemorizer.LogTrumpChange();
+            return PlayerAction.ChangeTrump();
+        }
 
-		protected PlayerAction PlayCard(Card card)
-		{
-			this.CardMemorizer.LogPlayedCard(card);
-			return PlayerAction.PlayCard(card);
-		}
+        protected PlayerAction PlayCard(Card card)
+        {
+            this.CardMemorizer.LogPlayedCard(card);
+            return PlayerAction.PlayCard(card);
+        }
 
-		protected PlayerAction CloseGame()
-		{
-			return PlayerAction.CloseGame();
-		}
+        protected PlayerAction CloseGame()
+        {
+            return PlayerAction.CloseGame();
+        }
 
-		protected virtual bool ShouldCloseGame(PlayerTurnContext context)
-		{
-			bool canCloseGame = this.PlayerActionValidator.IsValid(PlayerAction.CloseGame(), context, this.CardMemorizer.GetMyHand());
+        protected virtual bool ShouldCloseGame(PlayerTurnContext context)
+        {
+            bool canCloseGame = this.PlayerActionValidator.IsValid(PlayerAction.CloseGame(), context, this.CardMemorizer.GetMyHand());
 
-			if (canCloseGame)
-			{
-				int potentialPoints = this.MyPoints + this.CalcHandValue();
+            if (canCloseGame)
+            {
+                int potentialPoints = this.MyPoints + this.CalcHandValue();
 
-				return potentialPoints >= 60;
-			}
+                return potentialPoints >= 60;
+            }
 
-			return false;
-		}
+            return false;
+        }
 
-		protected int CalcHandValue()
-		{
-			return this.MyHand.Sum(card => card.GetValue());
-		}
-	}
+        protected int CalcHandValue()
+        {
+            return this.MyHand.Sum(card => card.GetValue());
+        }
+    }
 }
